2cc87a071f3470996655e74bdb387b5a398256a4
Armen Vardanyan <armenvardanyan95@gmail.com>
2020-03-06 20:12:47 +0400

    Documentation updates

--- src/filter/check-latest-value.operator.ts
+++ src/filter/check-latest-value.operator.ts
@@ -8,26 +8,22 @@ type Predicate<T> = (arg: T) => boolean;
 const identity = (e: boolean) => e;
 
 /**
- * Uses another stream's latest emission to determine whether to proceed with th source
+ * Uses another stream's latest emission to determine whether to proceed with the source
  * For example, in NGRX, proceed with an Effect when the user is logged in:
- * ```
- * class Effects {
  * 
- *  loadData$ = createEffect(() => this.actions.pipe(
- *          ofType(myAction),
- *          checkLatestFrom(this.store.select(state => state.user.isAuth)),
- *          // will proceed only when the user is signed in
- *  ));
- *  
- *  constructor(
- *      private readonly actions$: Actions,
- *      private readonly store: Store<AppState>,
- *  ) {}
- * }
- * ```
- * ```
- * <my-custom-element>Highlight JS will autodetect the language</my-custom-element>
- * ```
+ *      class Effects {
+ *      
+ *       loadData$ = createEffect(() => this.actions.pipe(
+ *               ofType(myAction),
+ *               checkLatestFrom(this.store.select(state => state.user.isAuth)),
+ *               // will proceed only when the user is signed in
+ *       ));
+ *       
+ *       constructor(
+ *           private readonly actions$: Actions,
+ *           private readonly store: Store<AppState>,
+ *       ) {}
+ *      }
 */
 export function checkLatestFrom<T extends boolean, S>(inner: Observable<T>, predicate: Predicate<T> = identity) {
     return function(source: Observable<S>): Observable<S> {
--- src/filter/truthy.operator.ts
+++ src/filter/truthy.operator.ts
@@ -1,6 +1,15 @@
 import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
+/**
+ * Filters truthy values:
+ * 
+ *  of(1, 0, null, 2, undefined, 0, 3)
+ *      .pipe(truthy())
+ *      .subscribe(console.log);
+ * 
+ * Will log `1, 2, 3`
+*/
 export function truthy<T>() {
     return function(source: Observable<T>): Observable<T> {
         return source.pipe(filter(item => !!item));
